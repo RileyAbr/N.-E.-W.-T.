19bba8728253869b8434471d6da9c3ff74dfb75f
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-07-20 18:45:36 -0500

    Secret code reader is now complete

--- newt.js
+++ newt.js
@@ -1,12 +1,31 @@
 import codes from "./secretCodes.js";
 
+const codeMessage = "Enter the secret codes:";
 const codeForm = document.getElementById("code-form");
+const codeLabel = document.getElementById("code-label");
 const numberOfCodes = codes.length;
 
+codeLabel.innerText = codeMessage;
+
+// Temporary changes the label text and restarts the animation
+const tempCodeLabelTextChange = (tempMessage) => {
+    codeLabel.style.animation = "none";
+    setTimeout(() => {
+        codeLabel.style.animation = "";
+    }, 10);
+
+    codeLabel.innerText = tempMessage;
+
+    setTimeout(() => {
+        codeLabel.innerText = codeMessage;
+    }, 4200);
+};
+
+//
 const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    let codeInput = document.getElementById("search-input");
+    let codeInput = document.getElementById("code-input");
     let codeInputValue = codeInput.value.toUpperCase();
 
     console.log(codeInputValue);
@@ -14,24 +33,23 @@ const handleFormSubmit = (event) => {
     if (codes.includes(codeInputValue)) {
         for (let i = 0; i < codes.length; i++) {
             if (codeInputValue === codes[i]) {
-                alert(`Secret Code #${i} discovered`);
-
-                document
-                    .getElementById(`secret-image-${i + 1}`)
-                    .style.setProperty("display", "block");
+                tempCodeLabelTextChange(
+                    `Congratulations, you found code #${i + 1}`
+                );
+                if (i + 1 == codes.length) {
+                    document
+                        .getElementById("finale-message")
+                        .style.setProperty("display", "block");
+                } else {
+                    document
+                        .getElementById(`secret-image-${i + 1}`)
+                        .style.setProperty("display", "block");
+                }
             }
         }
     } else {
-        alert(`This code is invalid. Please try again`);
+        tempCodeLabelTextChange("Error: Code is invalid");
     }
 };
 
 codeForm.addEventListener("submit", handleFormSubmit);
-
-const submitCode = (event) => {
-    let codeInput = document.getElementById("search-input");
-
-    event.preventDefault();
-
-    console.log(codeInput.value);
-};
