f6de8873208ac2ef6836cd5773c04ba09d9a46b6
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-07-20 19:58:08 -0500

    Added all image assets

--- secretCodes.js
+++ secretCodes.js
@@ -1,4 +1,23 @@
+// Scrambled codes to help keep track of what all is being done
+const scrambledCodes = [
+    "N3M7KEU", // Given in person
+    "PCE7R6U", // Caesar cipher with 25 as key
+    "", // ASL
+    "--... --- -.... -..- --.. . -..-", // Morse code
+    "", // QR Code
+    "CY3UENZ", // Backwards message
+    "01010010 01000100 00111000 01010101 00110001 01001110 01010101", // Binary
+];
+
 // Secret codes go here
-const codes = ["X", "Y", "Z"];
+const codes = [
+    "N3M7KEU",
+    "QDF7S6V",
+    "GDDWAO3",
+    "7O6XZEX",
+    "QBO2T8W",
+    "ZNEU3YC",
+    "RD8U1NU",
+];
 
 export default codes;
